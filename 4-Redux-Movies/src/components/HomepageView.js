74100e034a3c0c3ebaddc260fdbe76675f49b9ab
dmodalek <dominic@modalek.ch>
2018-01-08 17:12:58 +0100

    Add props to propTypes

--- 4-Redux-Movies/src/components/HomepageView.js
+++ 4-Redux-Movies/src/components/HomepageView.js
@@ -6,7 +6,13 @@ class HomepageView extends Component {
   static propTypes = {
     isLoading: PropTypes.bool.isRequired,
     movies: PropTypes.array.isRequired,
-    doLoadMovies: PropTypes.func.isRequired
+    doLoadMovies: PropTypes.func.isRequired,
+    doLoadMovieDetails: PropTypes.func.isRequired,
+    doAddToSelection: PropTypes.func.isRequired
+  };
+
+  state = {
+    error: false
   };
 
   componentDidMount() {
@@ -16,12 +22,19 @@ class HomepageView extends Component {
   render() {
     return (
       <div className="container">
+        {this.state.error ? (
+          <div style={{ color: "crimson" }}>Loading the movies failed! <span role="img" aria-label="Monkey">🙈</span></div>
+        ) : (
+          ""
+        )}
+        
         <h2>Movies</h2>
         {this.props.isLoading ? (
           "Loading..."
         ) : (
           <MovieList movies={this.props.movies} />
         )}
+
         <h2>Movies 3D</h2>
         {this.props.isLoading ? "Loading..." : <MovieList movies={[]} />}
       </div>
